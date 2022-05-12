d98022bfce32fefc852aa796b90fc094a5f8864d
3DRX <kjy20030228@icloud.com>
2022-05-12 19:21:57 +0800

    .

--- src/main.c
+++ src/main.c
@@ -10,24 +10,31 @@
 ENVIRONMENT * env;
 CAR * car;
 STATION * station;
+// TODO: 这三个将来可以放进main函数
 
 // #functions
 
-void initGame(void)
+void initGame(int TIME)
 {
     // init environment
-    FILE* fPtr= fopen("dict.dic", "r");
+    FILE* fPtr;
+    if ((fPtr = fopen("dict.dic", "r")) == NULL) {
+        printf("Can't open file \"dict.dic\"\n");
+        exit(EXIT_FAILURE);
+    }
     readfile(fPtr);
     // init car
     car->position = 0;
+    //init time
+    TIME = 0;
 }
 
 // #main
 
 int main(int argc, char**argv)
 {
-    int TIME = 0;
-    initGame();
+    int TIME;
+    initGame(TIME);
     return 0;
 }
 
