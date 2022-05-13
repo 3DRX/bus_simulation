6e058308d701443bfa9a95d7f6b347d35749fd7f
3DRX <kjy20030228@icloud.com>
2022-05-13 17:17:35 +0800

    polish code

--- src/input.c
+++ src/input.c
@@ -8,27 +8,22 @@ extern ENVIRONMENT env;
 void readfile(FILE * fPtr) // 从文件中读取环境初始配置
 {
     int workingIndex = 0;
+    // 自动机的状态
     enum {COMMENT, NORMAL, TOTAL_STATION, STRATEGY, DISTANCE} state;
-    char * lineBuffer = (char *)malloc(sizeof(char)* 100);
-
+    state = NORMAL;
     env.TOTAL_STATION = 5;
     env.STRATEGY = FCFS;
     env.DISTANCE = 2;
-    // start read file
     char ipt;
-    state = NORMAL;
+    // start read file
     while (1) {
         if ((fscanf(fPtr, "%c", &ipt)) == EOF) {
-            //printf("!! file end\n");
-            //printf("!! TOTAL_STATION is %d\n",env.TOTAL_STATION);
-            //printf("!! DISTANCE is %d\n",env.DISTANCE);
             break;
         }
         else {
             workingIndex++;
         }
         if (state == NORMAL) {
-            //printf("!! enter state: NORMAL\n");
             if (workingIndex == 1 && ipt == '#') {
                 state = COMMENT;
             }
@@ -46,22 +41,18 @@ void readfile(FILE * fPtr) // 从文件中读取环境初始配置
             }
         }
         else if (state == COMMENT) {
-            //printf("!! enter state: COMMENT\n");
             if (ipt == '\n') {
                 state = NORMAL;
                 workingIndex = 0;
             }
         }
         else if (state == TOTAL_STATION) {
-            //printf("!! enter state: TOTAL_STATION\n");
             if (ipt == '=') {
                 fscanf(fPtr, "%d", &env.TOTAL_STATION);
-                //printf("!! TOTAL_STATION changed to %d\n",env.TOTAL_STATION);
                 state = NORMAL;
             }
         }
         else if (state == STRATEGY) {
-            //printf("!! enter state: STRATEGY\n");
             if (ipt == '=') {
                 char inputBuf[10];
                 fscanf(fPtr, "%s", inputBuf);
@@ -78,15 +69,12 @@ void readfile(FILE * fPtr) // 从文件中读取环境初始配置
             }
         }
         else if (state == DISTANCE) {
-            //printf("!! enter state: DISTANCE\n");
             if (ipt == '=') {
                 fscanf(fPtr, "%d", &env.DISTANCE);
-                //printf("!! DISTANCE changed to %d\n",env.DISTANCE);
                 state = NORMAL;
             }
         }
     }
-
     // end read file
 }
 
