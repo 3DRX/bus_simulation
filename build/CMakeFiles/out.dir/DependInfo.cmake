5f6a6d16d556bbe29bb6478ff4a756090b5d8781
3DRX <kjy20030228@icloud.com>
2022-05-13 12:46:45 +0800

    （别管它）编译时的临时文件

---
+++ build/CMakeFiles/out.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kangjingyang/Documents/Bus_Operation/src/control.c" "/home/kangjingyang/Documents/Bus_Operation/build/CMakeFiles/out.dir/src/control.c.o"
+  "/home/kangjingyang/Documents/Bus_Operation/src/input.c" "/home/kangjingyang/Documents/Bus_Operation/build/CMakeFiles/out.dir/src/input.c.o"
+  "/home/kangjingyang/Documents/Bus_Operation/src/main.c" "/home/kangjingyang/Documents/Bus_Operation/build/CMakeFiles/out.dir/src/main.c.o"
+  "/home/kangjingyang/Documents/Bus_Operation/src/output.c" "/home/kangjingyang/Documents/Bus_Operation/build/CMakeFiles/out.dir/src/output.c.o"
+  "/home/kangjingyang/Documents/Bus_Operation/src/strategy.c" "/home/kangjingyang/Documents/Bus_Operation/build/CMakeFiles/out.dir/src/strategy.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/out.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../out"
+  "../out.pdb"
+  "CMakeFiles/out.dir/src/control.c.o"
+  "CMakeFiles/out.dir/src/input.c.o"
+  "CMakeFiles/out.dir/src/main.c.o"
+  "CMakeFiles/out.dir/src/output.c.o"
+  "CMakeFiles/out.dir/src/strategy.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
