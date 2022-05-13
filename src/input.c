e937daeda51c89566107b765d1ec96064d2ede07
3RDX <kjy20030228@icloud.com>
2022-05-13 11:01:35 +0800

    实现readfile函数

--- src/input.c
+++ src/input.c
@@ -4,11 +4,36 @@
 
 extern ENVIRONMENT env;
 
-void readfile(FILE * fPtr)
-{// 从文件中读取环境初始配置
+void readfile(FILE * fPtr) // 从文件中读取环境初始配置
+{
+    char station[] = "TOTAL_STATION";
+    char strategy[] = "STRATEGY";
+    char distance[] = "DISTANCE";
+    int workingIndex = 0;
+    enum {COMMENT, NORMAL} state;
+    char * lineBuffer = (char *)malloc(sizeof(char)* 100);
+
     env.TOTAL_STATION = 5;
     env.STRATEGY = FCFS;
     env.DISTANCE = 2;
     // start read file
+    char ipt;
+    state = NORMAL;
+    while (1) {
+        if ((fscanf(fPtr, "%c", &ipt)) == EOF) {
+            break;
+        }
+        if (state == NORMAL) {
+            if (workingIndex == 0 && ipt == '#') {
+                state = COMMENT;
+            }
+        }
+        else if (state == COMMENT) {
+            if (ipt == '\n') {
+                state = NORMAL;
+            }
+        }
+    }
+
     // end read file
 }
