cdfe214325a1f7a2232880967599dbb653c93b6e
3DRX <kjy20030228@icloud.com>
2022-05-24 19:05:02 +0800

    修改output模块中有关最大站点数的参数

--- src/output.c
+++ src/output.c
@@ -13,9 +13,9 @@ void printLines( void )
     printf( "TIME:%d\n", TIME );
     printf( "BUS:\n" );
     printf( "position:%d\n", car.position );
-    car.target[ 0 ][ 10 ] = -1; // 防越界
+    car.target[ 0 ][ 20 ] = -1; // 防越界
     printf( "target:" );
-    for ( int i = 0; i < 10; i++ ) {
+    for ( int i = 0; i < 20; i++ ) {
         if ( car.target[ 0 ][ i ] == -1 ) {
             break;
         }
@@ -23,10 +23,10 @@ void printLines( void )
     }
     printf( "\n" );
     printf( "STATION:\n" );
-    station.clockwise[ 0 ][ 10 ] = -1;        // 防越界
-    station.counterclockwise[ 0 ][ 10 ] = -1; // 防越界
+    station.clockwise[ 0 ][ 20 ] = -1;        // 防越界
+    station.counterclockwise[ 0 ][ 20 ] = -1; // 防越界
     printf( "clockwise:" );
-    for ( int i = 0; i < 10; i++ ) {
+    for ( int i = 0; i < 20; i++ ) {
         if ( station.clockwise[ 0 ][ i ] == -1 ) {
             break;
         }
@@ -34,7 +34,7 @@ void printLines( void )
     }
     printf( "\n" );
     printf( "counterclockwise:" );
-    for ( int i = 0; i < 10; i++ ) {
+    for ( int i = 0; i < 20; i++ ) {
         if ( station.counterclockwise[ 0 ][ i ] == -1 ) {
             break;
         }
