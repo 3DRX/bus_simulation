222213db73a08e6778bcf4cda3ce9ec93eed8e8c
3DRX <kjy20030228@icloud.com>
2022-06-07 16:10:40 +0800

    update runcode.sh

--- runcode.sh
+++ runcode.sh
@@ -1,10 +1,10 @@
-rm ./out
+rm ./Bus-operation
+rm -rf ./build
+mkdir build
 cd ./build
-rm -rf *
 cmake ..
 make
 cp ./compile_commands.json ../
 cd ..
-#./out
 ./Bus-operation > ./output.io < ./input.io
 
